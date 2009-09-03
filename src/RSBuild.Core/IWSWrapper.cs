0481a812f4fa4647528768a612a739830d028572
Gerke Geurts <ggeurts@users.noreply.github.com>
2009-09-03 12:55:18 +0000

    Added unit tests for Settings and Report classes. Significant refactoring of source code to improve testability.

---
+++ src/RSBuild.Core/IWSWrapper.cs
@@ -0,0 +1,12 @@
+﻿namespace RSBuild
+{
+    using System;
+    using RSBuild.Entities;
+
+    public interface IWsWrapper : IDisposable
+    {
+        void CreateFolder(string folder, string parent);
+        void CreateDataSource(DataSource source);
+        void CreateReport(Report report, string path, byte[] reportDefinition);
+    }
+}
